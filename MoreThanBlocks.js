d8433dafae9d6d9d1b7ea5b2d11cbe634493a528
peacestorm <rjanse48@gmail.com>
2014-10-08 17:09:08 +0200

    Added AgameR MoreThanBlocks

---
+++ MoreThanBlocks.js
@@ -0,0 +1,63 @@
+Block.defineBlock(25,"Iron Block 2x1","iron_block",0);
+Block.setShape(25,0,0,0,1,2,1);
+Player.addItemCreativeInv(25,1,0)
+
+Block.defineBlock(250,"Gold Block 2x1","gold_block",0);
+Block.setShape(250,0,0,0,1,2,1);
+Player.addItemCreativeInv(250,1,0)
+
+Block.defineBlock(251,"Diamond Block 2x1","diamond_block",0);
+Block.setShape(251,0,0,0,1,2,1);
+Player.addItemCreativeInv(251,1,0)
+
+Block.defineBlock(252,"Coal Block 2x1","coal_block",0);
+Block.setShape(252,0,0,0,1,2,1);
+Player.addItemCreativeInv(252,1,0)
+
+Block.defineBlock(253,"Lapis Block 2x1","lapis_block",0);
+Block.setShape(253,0,0,0,1,2,1);
+Player.addItemCreativeInv(253,1,0)
+
+Block.defineBlock(254,"Emerald Block 2x1","emerald_block",0);
+Block.setShape(254,0,0,0,1,2,1);
+Player.addItemCreativeInv(254,1,0)
+
+function newLevel() {
+    clientMessage(ChatColor.GREEN+"AgameR MoreThanBlocks PE"+ChatColor.RED+" 0.1 BETA "+ChatColor.WHITE+"by peacestorm initialized");
+};
+
+function procCmd(cmd) {
+    cmd = cmd.toLowerCase();
+    if (cmd == "changelog morethanblockspe"){
+        clientMessage("AgameR MoreThanBlocks PE Changelog - 0.1: Initial release");
+}};
+
+//*Copyright & Permissions*
+//-------------
+//You may:
+//Edit this mod for personal use
+//Create a review of this mod, as long as you give me credits and you put the link of this mod in the description
+//
+//You may NOT:
+//Distribute this mod without permission of peacestorm
+//Claim that this mod is yours
+//Include this mod in your modpack without permission of peacestorm
+//Use code of this mod in your own mod without permission of peacestorm
+//Monetize this mod by using AdFly or any other monetizing method
+//
+//For things that aren't listed here, always ask me
+
+
+//*Credits*
+//-----------
+//Creator: peacestorm
+
+//*Mod info*
+//------------
+//Mod version: 0.1 BETA
+//For full changelog, do /changelog morethanblockspe
+
+
+//*Ideas*
+//---------
+//If you have any ideas for this mod, please send them to my MC-Forum account, or on Twitter: @AgameR_Modder
