5ad9f8f71e2605ef2b9bf42aad4c6bc159a07403
peacestorm <rjanse48@gmail.com>
2014-10-10 19:33:51 +0200

    Added some slabs
    Added Iron, Gold, Diamond, Coal, Lapis and Emerald Slabs.

--- MoreThanBlocks.js
+++ MoreThanBlocks.js
@@ -22,14 +22,38 @@ Block.defineBlock(254,"Emerald Block 2x1","emerald_block",0);
 Block.setShape(254,0,0,0,1,2,1);
 Player.addItemCreativeInv(254,1,0)
 
+Block.defineBlock(143,"Iron Slab","iron_block",0);
+Block.setShape(143,0,0,0,0,0.5,0);
+Player.addItemCreativeInv(143,1,0)
+
+Block.defineBlock(144,"Gold Slab","gold_block",0);
+Block.setShape(144,0,0,0,0,0.5,0);
+Player.addItemCreativeInv(144,1,0)
+
+Block.defineBlock(145,"Diamond Slab","diamond_block",0);
+Block.setShape(145,0,0,0,0,0.5,0);
+Player.addItemCreativeInv(145,1,0)
+
+Block.defineBlock(146,"Coal Slab","coal_block",0);
+Block.setShape(146,0,0,0,0,0.5,0);
+Player.addItemCreativeInv(146,1,0)
+
+Block.defineBlock(147,"Lapis Slab","lapis_block",0);
+Block.setShape(147,0,0,0,0,0.5,0);
+Player.addItemCreativeInv(147,1,0)
+
+Block.defineBlock(148,"Emerald Slab","emerald_block",0);
+Block.setShape(148,0,0,0,0,0.5,0);
+Player.addItemCreativeInv(148,1,0)
+
 function newLevel() {
-    clientMessage(ChatColor.GREEN+"AgameR MoreThanBlocks PE"+ChatColor.RED+" 0.1 BETA "+ChatColor.WHITE+"by peacestorm initialized");
+    clientMessage(ChatColor.GREEN+"AgameR MoreThanBlocks PE"+ChatColor.RED+" 0.2 BETA "+ChatColor.WHITE+"by peacestorm initialized");
 };
 
 function procCmd(cmd) {
     cmd = cmd.toLowerCase();
     if (cmd == "changelog morethanblockspe"){
-        clientMessage("AgameR MoreThanBlocks PE Changelog - 0.1: Initial release");
+        clientMessage("AgameR MoreThanBlocks PE Changelog - 0.1 BETA: Initial release | 0.2 BETA: Added Iron, Gold, Diamond, Coal, Lapis and Emerald Slabs");
 }};
 
 //*Copyright & Permissions*
@@ -54,7 +78,7 @@ function procCmd(cmd) {
 
 //*Mod info*
 //------------
-//Mod version: 0.1 BETA
+//Mod version: 0.2 BETA
 //For full changelog, do /changelog morethanblockspe
 
 
