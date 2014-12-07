5598c36624b512e1a626993df615da55622eb9b9
peacestorm <rjanse48@gmail.com>
2014-12-07 21:22:15 +0100

    2.2 Build 1
    Added Mimic mob; Better Ender Cow skin; Some code improvements

--- AgameR MoreThanMobs PE.js
+++ AgameR MoreThanMobs PE.js
@@ -17,7 +17,7 @@ ModPE.setItem(506,"spawn_egg",6,"Spawn Pirate");
 ModPE.setItem(407,"spawn_egg",7,"Spawn Mimic");
 ModPE.setItem(434,"experience_bottle",0,"Experience Bottle");
 ModPE.setFoodItem(435,"potion_bottle_drinkable",0,20,"Healing Potion");
-ModPE.setFoodItem(436,"potion_bottle_empty",0,-11111,"Potion of Death");
+ModPE.setFoodItem(436,"potion_bottle_empty",0,0,"Potion of Death");
 
 //Spawn Eggs in creative!
 Player.addItemCreativeInv(500,1,0);
@@ -33,7 +33,7 @@ Player.addItemCreativeInv(435,1,0);
 Player.addItemCreativeInv(436,1,0);
 
 function newLevel() {
-    clientMessage(ChatColor.GREEN+"AgameR MoreThanMobs PE"+ChatColor.RED+" 2.2 "+ChatColor.WHITE+"by peacestorm initialized");
+    clientMessage(ChatColor.GREEN+"AgameR MoreThanMobs PE"+ChatColor.RED+" 2.2 (Build 1) "+ChatColor.WHITE+"by peacestorm initialized");
 };
 
 function procCmd(cmd) {
@@ -42,10 +42,10 @@ function procCmd(cmd) {
         clientMessage("AgameR MoreThanMobs PE Changelog - 1.0: Initial release | 1.1: Added the Ender Spider | 2.0: Added Ender Cow, Pirate, a healing potion, a potion of death and a experience bottle, name of the mod changed to AgameR MoreThanMobs | 2.1: Added /summon command and /disguise command | 2.2: Added Mimic mob; Improved spawn eggs; Better Ender Cow skin");
     }else if (cmd == "summon ghost"){
         var ghost = Level.spawnMob(Player.getX(), Player.getY(), Player.getZ(), 32, "mob/ghost.png");
-		Entity.setRenderType(ghost,3);
+		Entity.setRenderType(ghost,EntityRenderType.player);
 	}else if (cmd == "summon wolfgirl"){
         var wolfgirl = Level.spawnMob(Player.getX(), Player.getY(), Player.getZ(), 33, "mob/wolfgirl.png");
-		Entity.setRenderType(wolfgirl,3);
+		Entity.setRenderType(wolfgirl,EntityRenderType.player);
 	}else if (cmd == "summon bluecreeper"){
         var bluecreeper = Level.spawnMob(Player.getX(), Player.getY(), Player.getZ(), 33, "mob/blue_creeper.png");
 	}else if (cmd == "summon brownchicken"){
@@ -56,47 +56,47 @@ function procCmd(cmd) {
         var endercow = Level.spawnMob(Player.getX(), Player.getY(), Player.getZ(), 11, "mob/ender_cow.png");
 	}else if (cmd == "summon pirate"){
         var pirate = Level.spawnMob(Player.getX(), Player.getY(), Player.getZ(), 32, "mob/pirate.png");
-		Entity.setRenderType(pirate,3);
+		Entity.setRenderType(pirate,EntityRenderType.player);
 	}else if (cmd == "summon mimic"){
         var mimic = Level.spawnMob(Player.getX(), Player.getY(), Player.getZ(), 37, "mob/mimic.png");
 	}else if (cmd == "disguise ghost"){
         Entity.setMobSkin(Player.getEntity(), "mob/ghost.png");
-		Entity.setRenderType(Player.getEntity(), 3);
+		Entity.setRenderType(Player.getEntity(), EntityRenderType.player);
 	}else if (cmd == "disguise wolfgirl"){
         Entity.setMobSkin(Player.getEntity(), "mob/wolfgirl.png");
-		Entity.setRenderType(Player.getEntity(), 3);
+		Entity.setRenderType(Player.getEntity(), EntityRenderType.player);
 	}else if (cmd == "disguise bluecreeper"){
         Entity.setMobSkin(Player.getEntity(), "mob/blue_creeper.png");
-		Entity.setRenderType(Player.getEntity(), 17);
+		Entity.setRenderType(Player.getEntity(), EntityRenderType.creeper);
 	}else if (cmd == "disguise brownchicken"){
         Entity.setMobSkin(Player.getEntity(), "mob/brown_chicken.png");
-		Entity.setRenderType(Player.getEntity(), 5);
+		Entity.setRenderType(Player.getEntity(), EntityRenderType.chicken);
 	}else if (cmd == "disguise enderspider"){
         Entity.setMobSkin(Player.getEntity(), "mob/ender_spider.png");
-		Entity.setRenderType(Player.getEntity(), 15);
+		Entity.setRenderType(Player.getEntity(), EntityRenderType.spider);
 	}else if (cmd == "disguise endercow"){
         Entity.setMobSkin(Player.getEntity(), "mob/ender_cow.png");
-		Entity.setRenderType(Player.getEntity(), 6);
+		Entity.setRenderType(Player.getEntity(), EntityRenderType.cow);
 	}else if (cmd == "disguise pirate"){
         Entity.setMobSkin(Player.getEntity(), "mob/pirate.png");
-		Entity.setRenderType(Player.getEntity(), 3);
+		Entity.setRenderType(Player.getEntity(), EntityRenderType.player);
 	}else if (cmd == "disguise mimic"){
         Entity.setMobSkin(Player.getEntity(), "mob/mimic.png");
-		Entity.setRenderType(Player.getEntity(), 10);
+		Entity.setRenderType(Player.getEntity(), EntityRenderType.slime);
     }
 };
 
 function useItem(x, y, z, itemId, blockId, side){
 if(itemId == 500){var ghost = Level.spawnMob(x,y+1,z,32,"mob/ghost.png");
-Entity.setRenderType(ghost,3);
+Entity.setRenderType(ghost,EntityRenderType.player);
 }else if(itemId == 501){var wolfgirl = Level.spawnMob(x,y+1,z,33,"mob/wolfgirl.png");
-Entity.setRenderType(wolfgirl,3);
+Entity.setRenderType(wolfgirl,EntityRenderType.player);
 }else if(itemId == 502){var bluecreeper = Level.spawnMob(x,y+1,z,33,"mob/blue_creeper.png");
 }else if(itemId == 503){var brownchicken = Level.spawnMob(x,y+1,z,10,"mob/brown_chicken.png");
 }else if(itemId == 504){var enderspider = Level.spawnMob(x,y+1,z,35,"mob/ender_spider.png");
 }else if(itemId == 505){var endercow = Level.spawnMob(x,y+1,z,11,"mob/ender_cow.png");
 }else if(itemId == 506){var pirate = Level.spawnMob(x,y+1,z,32,"mob/pirate.png");
-Entity.setRenderType(pirate,3);
+Entity.setRenderType(pirate,EntityRenderType.player);
 }else if(itemId == 407){var mimic = Level.spawnMob(x,y+1,z,37,"mob/mimic.png");
 }else if(itemId == 434){var experiencebottle = setVelY(Player.getEntity(),5);
 }else if(itemId == 435){var healingpotion = Player.setHealth(20);
@@ -104,11 +104,11 @@ Entity.setRenderType(pirate,3);
 }
 };
 
-function deathHook(attacker,victim){
-if(victim==ghost){
-Level.dropItem(Entity.getX(victim), Entity.getY(victim), Entity.getZ(victim), 1, 434, 1, 0);
-}
-};
+/*function attackHook(attacker,victim){
+if(Player.getCarriedItem() == itemId){
+var render = Entity.getRenderType(victim);
+clientMessage("Rendertype: "+render);
+}}*/
 
 function leaveGame(){
 }
@@ -136,7 +136,7 @@ function leaveGame(){
 
 //*Mod info*
 //------------
-//Mod version: 2.2
+//Mod version: 2.2 (Build 1)
 //For full changelog, do /changelog morethanmobspe
 
 //*Todo-list*
