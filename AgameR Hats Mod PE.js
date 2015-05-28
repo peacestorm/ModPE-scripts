916d6f859a425a6f82d971b3197fc882fdcad5b6
peacestorm <rjanse48@gmail.com>
2015-05-28 17:38:12 +0200

    1.2.1

--- AgameR Hats Mod PE.js
+++ AgameR Hats Mod PE.js
@@ -5,7 +5,7 @@ ModPE.overrideTexture("images/mob/char_sand.png","http://i.imgur.com/kQRdWkf.png
 ModPE.overrideTexture("images/mob/char_wood.png","http://i.imgur.com/UXDvSJx.png");
 ModPE.overrideTexture("images/mob/char_woodenplanks.png","http://i.imgur.com/uTLc2GE.png");
 ModPE.overrideTexture("images/mob/char_chest.png","http://i.imgur.com/COq01OY.png");
-ModPE.overrideTexture("images/mob/char_glass.png","http://i.imgur.com/cMgjgLm.png");
+ModPE.overrideTexture("images/mob/char_glass.png","http://i.imgur.com/cX3atMD.png");
 ModPE.overrideTexture("images/mob/char_cobblestone.png","http://i.imgur.com/ipsdv2g.png");
 ModPE.overrideTexture("images/mob/char_bedrock.png","http://i.imgur.com/1IgjViJ.png");
 ModPE.overrideTexture("images/mob/char_obsidian.png","http://i.imgur.com/Blh6h1c.png");
@@ -34,8 +34,21 @@ ModPE.overrideTexture("images/mob/char_present.png","http://i.imgur.com/r1ndlvu.
 ModPE.overrideTexture("images/mob/char_popcorn.png","http://i.imgur.com/xAxzHBt.png");
 ModPE.overrideTexture("images/mob/char_nyancat.png","http://i.imgur.com/KtfJ4pz.png");
 
+Entity.setHat = function(hat,entity){
+if(hat==null){
+if(entity==null){
+Entity.setMobSkin(Player.getEntity(),"mob/char.png");
+}else{
+Entity.setMobSkin(entity,"mob/char.png");
+}}else{
+Entity.setMobSkin(entity,hat);
+}
+}
+
 var GUI;
 var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
+var initialized = false;
 
 function newLevel() {
    /*ctx.runOnUiThread(new java.lang.Runnable(){
@@ -92,92 +105,101 @@ function newLevel() {
 }
                      });
 
-    */clientMessage(ChatColor.GREEN+"AgameR Hats Mod PE"+ChatColor.RED+" 1.2 "+ChatColor.WHITE+"by peacestorm initialized");
+    */if(initialized == false){
+		clientMessage(ChatColor.GREEN+"AgameR Hats Mod PE"+ChatColor.RED+" 1.2.1 "+ChatColor.WHITE+"by peacestorm initialized");
+		initialized = true;
+	}else if(initialized == true){
+		clientMessage(ChatColor.GREEN+"AgameR Hats Mod PE"+ChatColor.RED+" 1.2.1 "+ChatColor.WHITE+"by peacestorm");
+	}
 };
 
 function modTick() {
-//ModPE.showTipMessage("AgameR Hats Mod PE 1.2 Build 2");
+//ModPE.showTipMessage("AgameR Hats Mod PE 1.2 Build 2"); <-- Beta builds
+//ModPE.showTipMessage(Entity.getMobSkin(Player.getEntity())); <-- Current hat
 };
 
 function procCmd(cmd) {
-    cmd = cmd.toLowerCase();
-    if (cmd == "changelog hatsmodpe"){
-        clientMessage("AgameR Hats Mod PE Changelog - 1.0: Initial release | 1.1: Added much more block hats; Improved some hats | 1.2 - The christmas update: Added more hats; Improved some hats");
-    }else if (cmd == "hat set grass"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_grass.png");
-    }else if (cmd == "hat set dirt"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_dirt.png");
-    }else if (cmd == "hat set stone"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_stone.png");
-    }else if (cmd == "hat set sand"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_sand.png");
-    }else if (cmd == "hat set wood"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_wood.png");
-    }else if (cmd == "hat set woodenplanks"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_woodenplanks.png");
-    }else if (cmd == "hat set chest"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_chest.png");
-    }else if (cmd == "hat set glass"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_glass.png");
-    }else if (cmd == "hat set cobblestone"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_cobblestone.png");
-    }else if (cmd == "hat set bedrock"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_bedrock.png");
-    }else if (cmd == "hat set obsidian"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_obsidian.png");
-    }else if (cmd == "hat set cactus"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_cactus.png");
-    }else if (cmd == "hat set glowstone"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_glowstone.png");
-    }else if (cmd == "hat set ice"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_ice.png");
-    }else if (cmd == "hat set netherrack"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_netherrack.png");
-    }else if (cmd == "hat set snow"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_snow.png");
-    }else if (cmd == "hat set sponge"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_sponge.png");
-    }else if (cmd == "hat set gravel"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_gravel.png");
-    }else if (cmd == "hat set bricks"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_bricks.png");
-    }else if (cmd == "hat set mushroom"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_mushroom.png");
-    }else if (cmd == "hat set tnt"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_tnt.png");
-    }else if (cmd == "hat set santahat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_santahat.png");
-    }else if (cmd == "hat set elfhat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_elfhat.png");
-    }else if (cmd == "hat set villagerhat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_villagerhat.png");
-    }else if (cmd == "hat set creeperhat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_creeperhat.png");
-    }else if (cmd == "hat set pighat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_pighat.png");
-    }else if (cmd == "hat set zombiehat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_zombiehat.png");
-	}else if (cmd == "hat set herobrinehat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_herobrinehat.png");
-    }else if (cmd == "hat set 3dglasses"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_3dglasses.png");
-    }else if (cmd == "hat set froghat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_froghat.png");
-    }else if (cmd == "hat set cap"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_cap.png");
-    }else if (cmd == "hat set tophat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_tophat.png");
-    }else if (cmd == "hat set present"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_present.png");
-    }else if (cmd == "hat set popcorn"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_popcorn.png")
-    }else if (cmd == "hat set nyancat"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char_nyancat.png")
-    }else if (cmd == "hat reset"){
-        Entity.setMobSkin(Player.getEntity(),"mob/char.png");
-    }else if (cmd == "hat types"){
+    cmd = cmd.split(" ");
+    if (cmd[0] == "changelog"){
+	    if (cmd[1] == "hatsmodpe"){
+        clientMessage("AgameR Hats Mod PE Changelog - 1.0: Initial release | 1.1: Added much more block hats; Improved some hats | 1.2 - The christmas update: Added more hats; Improved some hats | 1.2.1: Fixed some bugs; Some improvements");
+	}}else if (cmd[0] == "hat"){
+	if (cmd[1] == "set"){
+	if (cmd[2] == "grass"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "dirt"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "stone"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "sand"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "wood"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "woodenplanks"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "chest"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "glass"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "cobblestone"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "bedrock"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "obsidian"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "cactus"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "glowstone"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "ice"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "netherrack"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "snow"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "sponge"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "gravel"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "bricks"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "mushroom"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "tnt"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "santahat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "elfhat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "villagerhat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "creeperhat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "pighat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "zombiehat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+	}else if (cmd[2] == "herobrinehat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "3dglasses"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "froghat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "cap"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "tophat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "present"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "popcorn"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }else if (cmd[2] == "nyancat"){
+        Entity.setHat("mob/char_" + cmd[2] + ".png",Player.getEntity());
+    }}else if (cmd[1] == "reset"){
+        Entity.setHat("mob/char.png",Player.getEntity());
+	}else if (cmd[1] == "types"){
         clientMessage("grass, dirt, stone, sand, wood, woodenplanks, chest, glass, cobblestone, bedrock, obsidian, cactus, glowstone, ice, netherrack, snow, sponge, gravel, bricks, mushroom, tnt, santahat, elfhat, villagerhat, creeperhat, pighat, zombiehat, herobrinehat, 3dglasses, froghat, cap, tophat, present, popcorn, nyancat");
-}};
+}}};
 
 //*Copyright & Permissions*
 //-------------
@@ -201,7 +223,7 @@ function procCmd(cmd) {
 
 //*Mod info*
 //------------
-//Mod version: 1.2
+//Mod version: 1.2.1
 //For full changelog, do /changelog hatsmodpe
 
 
